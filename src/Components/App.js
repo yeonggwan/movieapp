ce35fb5d5bb0ed5f017e8051ff91bd4f90d2a0df
yeonggwan <hjk781@gmail.com>
2019-06-28 10:21:57 +0900

    Setting and React Router

---
+++ src/Components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Router from "../Components/Router";
+import Header from "./Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Router />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Header.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export default () => (
+  <header>
+    <ul>
+      <li>
+        <a href="/">Movies</a>
+      </li>
+      <li>
+        <a href="/tv">TV</a>
+      </li>
+      <li>
+        <a href="/Search">Search</a>
+      </li>
+    </ul>
+  </header>
+);
