247597321a4509d62dfff7a4ee2dbf9e2ab1ee44
yeonggwan <hjk781@gmail.com>
2019-07-03 03:29:03 +0900

    Presenter setting

--- src/Routes/Home/HomePresenter.js
+++ src/Routes/Home/HomePresenter.js
@@ -1,1 +1,16 @@
-export default () => "Home";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const HomePresenter = ({ nowPlaying, popular, upComing, error, loading }) =>
+  null;
+
+HomePresenter.propTypes = {
+  nowPlaying: PropTypes.array,
+  popular: PropTypes.array,
+  upComing: PropTypes.array,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired
+};
+
+export default HomePresenter;
--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -1,1 +1,23 @@
-export default () => "Search";
+import React from "react";
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const SearchPresenter = ({
+  movieResults,
+  tvResults,
+  searchTerm,
+  handleSubmit,
+  error,
+  loading
+}) => null;
+
+SearchPresenter.propTypes = {
+  movieResults: PropTypes.array,
+  tvResults: PropTypes.array,
+  searchTerm: PropTypes.string,
+  handleSubmit: PropTypes.func.isRequired,
+  error: PropTypes.string,
+  loading: PropTypes.bool.isRequired
+};
+
+export default SearchPresenter;
